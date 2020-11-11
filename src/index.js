470f8ccb7d31b7b7a972b68eaf6f9f53f267a423
alonghuayu <alonghuayu@163.com>
2020-11-12 00:46:47 +0800

    feature index.js

--- src/index.js
+++ src/index.js
@@ -15,3 +15,7 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+// React负责逻辑控制，数据 -> VDOM
+// ReactDom渲染实际DOM，VDOM -> DOM
+// React使⽤JSX来描述UI
+// babel-loader把JSX 编译成相应的 JS 对象，React.createElement再把这个JS对象构造成React需要的虚拟dom。
